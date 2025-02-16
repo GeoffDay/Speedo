36f3257e4dfaf42866efefe7af6ae58334d1d470
Geoff Day <gda26470@gmail.com>
2025-02-16 19:46:10 +1030

    Update Speedo.ino
    This is using the 128 x 64 OLED display with the 16x1 LCD removed. Not perfect yet but does work.

--- Speedo.ino
+++ Speedo.ino
@@ -1,77 +1,70 @@
 // The Suzuki Vitara has a crap dash. It's low contraast and hard to read in bright sun at certain angles. 
 // There are a number of displays for the fuel miser and even a blank one but no digital speed readout.
-// This connects a basic GPS sensor to a Arduino Uno and drives a I2C LCD display. I use TinyGPSPlus to 
-// parse the RMC message and display it on the LCD. I'm using a 16 x 2 line and deriving a direction too.
-// When I'm happy with it I may look at a better display, either an 8 x 1 or perhaps a 128 x 64 pixel OLED 
-/*
-   This sample code demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
-*/
-#include <TinyGPSPlus.h>
-#include <SoftwareSerial.h>
-//
-//#include <LiquidCrystal_I2C.h>
-#include <LiquidCrystal.h>        // include the library code:
+// This connects a basic GPS sensor to a Arduino Uno and drives a I2C 128 x 64 pixel OLED display. 
+// I use TinyGPSPlus to parse the RMC message and display it. 
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// // initialize the library by associating any needed LCD interface pin
+// // with the arduino pin number it is connected to
+// #include <LiquidCrystal.h>        // include the library code:
+// const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 /* Arduino UNO with SSD1306 128x64 I2C OLED display
  * Demonstrates graphics and text elements. Tony Goodhew  26 May 2020  */
-#include <SPI.h>           // Not needed with I2C
+#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin  
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin  
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// Initialise variables
+int cw = SSD1306_WHITE;
+int cb = SSD1306_BLACK;
 
-void show() { // Often used sequence - Function to simplify code
-  display.display(); delay(200); display.fillScreen(SSD1306_BLACK);
-}
+int x = 20;
+int y = 10;
 
+
+#include <SoftwareSerial.h>
 static const int RXPin = 10, TXPin = 9;
-static const uint32_t GPSBaud = 9600;        // my GPS is outputting at 9600 baud
+static const uint32_t GPSBaud = 9600;         // my GPS is outputting at 9600 baud
 
-// The TinyGPSPlus object
-TinyGPSPlus gps;
+#include <TinyGPSPlus.h>
+TinyGPSPlus gps;                              // The TinyGPSPlus object
 
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);              // I'm sure I could use the other serial pins. later perhaps...
 char _buffer[82];            // for the speed
 char dir[3] = "..";          // direction
 float speed = 0.0;
-float oldSpeed = 0.0;
-  // Initialise variables
-  int cw = SSD1306_WHITE;
-  int cb = SSD1306_BLACK;
+int oldSpeed = 100;
 
-  int x = 20;
-  int y = 10;
+int ledPin = 13;      // select the pin for the LED
 
 void setup()
-{
+{  
+  pinMode(ledPin, OUTPUT);    // declare the ledPin as an OUTPUT:
   ss.begin(GPSBaud);
+  delay(1000);
 
-  lcd.begin(8, 2);               // set up the 4 wire LCD's number of columns and rows:
-  lcd.display();
-
-  lcd.print("Hello");
+  digitalWrite(ledPin, LOW); // turn the ledPin off to show error
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Old Address 0x3D for 128x64
-    lcd.print(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {  // Old Address 0x3D for 128x64
+    digitalWrite(ledPin, HIGH); // turn the ledPin on to show error
+  } 
+
+  display.display();
+  delay(1000); // Pause for 2 seconds
 
   display.clearDisplay();
-  display.setTextSize(6);
+  display.setTextSize(3);
 
   smartDelay(100);
 }
@@ -92,8 +85,11 @@ void loop()
   if (speed < 0.5) strcpy(dir, "--");
 
   dtostrf(speed,3, 0, _buffer);      // convert float to a string -> _buffer
+  sprintf(_buffer, "%s %s ", _buffer, dir);  // create the string for the lcd & add direction
 
-  if (speed != oldSpeed) {
+  int intSpeed = (int)speed * 10;
+
+  if (intSpeed != oldSpeed) {
     display.fillRect(x,y, 127, 63, cb); // Overwrite with black rectangle
     display.display();
     display.setCursor(x, y);
@@ -102,18 +98,18 @@ void loop()
     display.display();
   } 
   
-  sprintf(_buffer, "%s kph ", _buffer);  // create the string for the lcd & add direction
-
-  displayOnLCD(_buffer, dir, 100);
-
-
-
-
-  oldSpeed = speed;       // get a copy of the current speed
+  oldSpeed = intSpeed;       // get a copy of the current speed
 
   smartDelay(10);
 
-  if (millis() > 5000 && gps.charsProcessed() < 10) displayOnLCD(0, "No GPS found", 1000);
+  // if (millis() > 5000 && gps.charsProcessed() < 10) {
+  //   display.fillRect(x,y, 127, 63, cb); // Overwrite with black rectangle
+  //   display.display();
+  //   display.setCursor(x, y);
+  //   display.setTextColor(cw);
+  //   display.println("no GPS");
+  //   display.display();
+  // } 
 }
 
 
@@ -130,22 +126,3 @@ static void smartDelay(unsigned long ms)
 }
 
 
-
-void displayOnLCD(int row, String charArray, int dDelay)
-{
-  lcd.clear();
-  lcd.setCursor(0, row); 
-  lcd.print(charArray);
-  smartDelay(dDelay);
-}
-
-
-void displayOnLCD(String charArray, String char_Array, int dDelay)
-{
- // lcd.clear();
-  lcd.setCursor(0, 0); 
-  lcd.print(charArray);
-  lcd.setCursor(0, 1); 
-  lcd.print(char_Array);
-  smartDelay(dDelay);
-}
